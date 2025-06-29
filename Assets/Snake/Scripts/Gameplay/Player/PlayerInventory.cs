77a4dae2e7d73f05f8749b2da9eb2f00950f55ad
xidiel <stormingisle@proton.me>
2025-06-29 14:09:09 +0300

    Inventory; Pickup Interactable

--- Assets/Snake/Scripts/Gameplay/Player/PlayerInventory.cs
+++ Assets/Snake/Scripts/Gameplay/Player/PlayerInventory.cs
@@ -13,12 +13,15 @@ namespace Snake {
 
         public void Add(InventoryItem item) {
             _items.Add(item);
+            
+            ItemAdded.Invoke(item);
         }
 
         public bool TryUse(string name) {
             var foundItem = _items.Find(x => x.name == name);
 
             if (foundItem != null) {
+                ItemRemoved.Invoke(foundItem);
                 _items.Remove(foundItem);
                 return true;
             }
---
+++ Assets/Snake/Scripts/Gameplay/UI/InventoryGridItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Snake {
+    public class InventoryGridItem :  MonoBehaviour {
+        
+        
+        public Image image;
+        public string itemName;
+    }
+}
