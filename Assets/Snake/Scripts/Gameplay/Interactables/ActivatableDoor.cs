b74e349f26038511ab771e22aa4b78ba936f3b69
xidiel <stormingisle@proton.me>
2025-06-29 18:13:05 +0300

    Door works

--- Assets/Snake/Scripts/Gameplay/Interactables/ActivatableDoor.cs
+++ Assets/Snake/Scripts/Gameplay/Interactables/ActivatableDoor.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 namespace Snake {
     public class ActivatableDoor : ActivatableBase {
         [SerializeField] Collider _collider;
+        
+        [SerializeField] Animator _animator;
 
         public override void Activate() {
             _collider.enabled = !_collider.enabled;
+            _animator.SetTrigger("Slide");
         }
     }
 }
